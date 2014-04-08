d0cd2c7f5cf1a7397e299d6278d9c19688f05c88
Alex Kavanagh <alex@ajkavanagh.co.uk>
2014-04-08 15:58:40 +0100

    Initial step-1 test with no code

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,7 +1,11 @@
+from __future__ import absolute_import
+
 import unittest
 
+from cf_converter import c_to_f
 
 class Test_CF_Converter(unittest.TestCase):
 
     # Write a test that fails!
-    pass
+    def test_boiling_point(self):
+        self.assertEqual(c_to_f(100), 212)
